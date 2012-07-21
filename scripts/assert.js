66ba84315dcb8c11307f487e8955869c915aa91a
Epic720 <Epic720@gmail.com>
2012-07-21 10:09:03 -0700

    removed some files that were unneeded.

--- scripts/assert.js
+++
@@ -1,9 +0,0 @@
-var output = document.getElementById('output');
-
-function assert( outcome, description ) {
-    var li = document.createElement('li');
-    li.className = outcome ? 'pass' : 'fail';
-    li.appendChild( document.createTextNode( description ) );
-
-    output.appendChild(li);
-};
